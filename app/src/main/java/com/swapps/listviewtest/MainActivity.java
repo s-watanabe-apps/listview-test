d9fec60be853bdf50f1d70230e9ff9600660a044
s-watanabe <swata82@gmail.com>
2021-10-22 17:00:07 +0900

    judgement of last line.

--- app/src/main/java/com/swapps/listviewtest/MainActivity.java
+++ app/src/main/java/com/swapps/listviewtest/MainActivity.java
@@ -3,7 +3,10 @@ package com.swapps.listviewtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.AbsListView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     ListView listView;
 
     ListAdapter listAdapter;
+
+    Boolean isLastLine = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +32,28 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.list_view);
         listAdapter = new ListAdapter(this, R.layout.list_item, listItems);
         listView.setAdapter(listAdapter);
+
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView absListView, int i) {
+                //
+            }
+
+            @Override
+            public void onScroll(AbsListView absListView,
+                                 int firstVisibleItem,
+                                 int visibleItemCount,
+                                 int totalItemCount) {
+
+                if (isLastLine) {
+                    isLastLine = (totalItemCount == firstVisibleItem + visibleItemCount);
+                } else {
+                    isLastLine = (totalItemCount == firstVisibleItem + visibleItemCount);
+                    if (isLastLine) {
+                        Toast.makeText(MainActivity.this, "最終行", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 }
